b7e8d5c4f91282544d40db14deb27a332da25e72
Nico Pasqualini <pasqualinin9@gmail.com>
2022-05-31 15:28:01 +1000

    seeding and server running

---
+++ seeds/car-seeds.js
@@ -0,0 +1,40 @@
+const Car = require('../models/Car');
+
+const carData = [
+
+    {
+        title: "Focus",
+        manufacturer: "Ford",
+        description: "wonderful ford focus",
+        price: 10000,
+        mileage: 200000,
+        user_id: 3
+    },
+    { title: "Commodore",
+    manufacturer: "Holden",
+    description: "wonderful commodore",
+    price: 5000,
+    mileage: 250000,
+    user_id: 2
+    },
+    { title: "A3",
+    manufacturer: "Audi",
+    description: "wonderful audi",
+    price: 15000,
+    mileage: 111000,
+    user_id: 3
+    },
+    { title: "Zafira",
+    manufacturer: "Opel",
+    description: "wonderful opel",
+    price: 1000,
+    mileage: 400000,
+    user_id: 1
+    },
+
+
+];
+
+const seedCars = () => Car.bulkCreate(carData);
+
+module.exports = seedCars;
---
+++ seeds/user-seeds.js
@@ -0,0 +1,29 @@
+const User = require('../models/User');
+
+const userData = [
+    {
+        name: "Joe Kalston",
+        email: "joekalston@yahoo.com",
+        password: "password1"
+    },
+    {
+        name: "Jane Doe",
+        email: "janedoe@fakemail.com",
+        password: "password2"
+    },
+    {
+        name: "Kenneth Sy",
+        email: "kensy@gmail.com",
+        password: "password3"
+    },
+    
+   
+];
+
+const seedUsers = () => User.bulkCreate(userData,{
+    individualHooks: true,
+    returning: true,
+  
+});
+
+module.exports = seedUsers;
